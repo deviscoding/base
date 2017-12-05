ec35b5855881b9e33d4930e01189326374bc1b90
AMJones <aaronj@jonesiscoding.com>
2017-12-04 18:52:12 -0600

    Adds DomSearch and SearchField plugins.

---
+++ js/dom-search.js
@@ -0,0 +1,36 @@
+/*!
+ * Searches the given selector for the given text value, then performs the given callback.
+ *
+ * @version Strapless DomSearch v1.1.4
+ * @license http://github.com/strapless/base/LICENSE
+ * @author  AMJones <am@jonesiscoding.com>
+ * @returns {jQuery}
+ */
+jQuery.fn.extend( {
+  domSearch: function ( sel, value, callback ) {
+    var plugin = this;
+    plugin.value = value;
+    plugin.settings = { minCharacter: 2 };
+
+    plugin.hasSearch = function(elem) {
+      var retval = false;
+      $(elem).contents().each(function() {
+        var text = plugin.getText(this);
+        if(text && text.indexOf(plugin.value.toLowerCase()) >= 0) {
+          retval = true;
+        }
+      });
+
+      return retval;
+    };
+
+    plugin.getText = function(elem) {
+      var text = (elem.textContent || elem.innerText || $(elem).text() || "").toLowerCase();
+      return (text.length >= plugin.settings.minCharacter) ? text : null;
+    };
+
+    $(sel).each(function() {
+      callback(this,plugin.hasSearch(this));
+    });
+  }
+} );
