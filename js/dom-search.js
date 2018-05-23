c0ab3e9884eaa9fdef8efe05f181dd1fa94afc9f
Aaron M Jones <aaron@jonesiscoding.com>
2018-05-23 10:14:16 -0500

    Version bump.

--- js/dom-search.js
+++ js/dom-search.js
@@ -1,5 +1,5 @@
 /**
- * DevIsCoding DomSearch v1.1.4 --
+ * DevIsCoding DomSearch v1.1.13 --
  * Searches the given selector for the given text value, then performs the given callback.
  *
  * @returns {jQuery}
